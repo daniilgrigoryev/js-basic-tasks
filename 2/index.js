845033362b0be2429104ad2df0ec791f535ff554
Daniil Grigoryev <grigorev@stdpr.ru>
2019-04-17 17:51:16 +0300

    small changes

--- 2/index.js
+++ 2/index.js
@@ -1,22 +1,13 @@
 let getCurrentDate = new Date();
 
 let getCurrentDay = getCurrentDate.getDate();
-let getCurrentMonth = getCurrentDate.getMonth();
+let getCurrentMonth = getCurrentDate.getMonth() + 1;
 let getFullYear = getCurrentDate.getFullYear();
 
 let formatDate = function(params, mask){
-
-	let month = params.month < 10 ? `0${params.month}`: params.month;
-	let day = params.day < 10 ? `0${params.day}`: params.day;
-	let year = params.year;
-
-	// let chipsMask = mask.split(/\.|\-|\//);
-	// let matchDevideres = mask.match(/\.|\-|\//ig);
-
-	// let dateFormated; 
-
-	mask = mask.replace(/yyyy/, year).replace(/dd/, day).replace(/mm/, month);
-
+	params.month = params.month < 10 ? `0${params.month}`: params.month;
+	params.day = params.day < 10 ? `0${params.day}`: params.day;
+	mask = mask.replace(/yyyy/, params.year).replace(/dd/, params.day).replace(/mm/, params.month);
 	return mask;
 };
 
