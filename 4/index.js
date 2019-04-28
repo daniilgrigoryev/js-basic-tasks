eba3a3033642e42090a5bfc7f4e237cc37e50834
Daniil Grigoryev <daniil.grigoryev@protonmail.com>
2019-04-28 13:50:50 +0300

    my best decision using ES5 methods

--- 4/index.js
+++ 4/index.js
@@ -1,14 +1,12 @@
 function findAreaTriangle(sides) {
   const perimeter = sides.reduce((sum, a) => sum + a) / 2;
-
   let multiply = sides.reduce((sum, curr, i, arr) => {
-    arr[i] = perimeter - curr;
-    return arr;
-  }, 0).reduce((sum, a) => sum * a);
-
-  return Math.sqrt(multiply * perimeter);
+    arr.splice(i, 1, perimeter - curr);
+    return arr.reduce((sum, a) => sum * a);;
+  }, 0) * perimeter;
+  return Math.sqrt(multiply);
 }
 
-let area = findAreaTriangle([5, 6, 7]);
+let area = findAreaTriangle([4, 5, 6]);
 
 console.log(area);
