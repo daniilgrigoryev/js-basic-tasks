880789c17c6576c3038f7218700647f2ef1ae26d
Daniil Grigoryev <grigorev@stdpr.ru>
2019-04-30 13:50:35 +0300

    done 7 task

---
+++ 7/index.js
@@ -0,0 +1,22 @@
+function yearsFirstJanSun(startYear, endYear){
+  let listYears = [];
+
+  // do{
+  //   const getDate = new Date(startYear, 0, 1);
+  //   if(getDate.getDay() == 0) listYears.push(startYear);
+  //   startYear++;
+  // }while(startYear < endYear);
+
+  for(startYear; startYear < endYear; startYear++){
+    const getDate = new Date(startYear, 0, 1);
+    getDate.getDay() == 0 ? listYears.push(startYear) : undefined;
+  }
+  return listYears;
+}
+
+
+let listYear = yearsFirstJanSun(2000, 2050);
+
+
+
+console.log(listYear);
---
+++ 7/readme.md
@@ -0,0 +1,1 @@
+### Find 1st January be a Sunday between a range of years
